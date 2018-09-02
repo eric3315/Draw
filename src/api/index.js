31f9a8624f37c36e15ff09b4f3e94bf581c7095e
v_yhuiwei <v_yhuiwei>
2018-09-02 10:33:34 +0800

    'build'

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Qs from 'qs';
-axios.defaults.baseURL = 'http://test.admin.skepay.com/';
+// axios.defaults.baseURL = 'http://test.admin.skepay.com/';
+axios.defaults.baseURL = 'http://127.0.0.1:8080';
 // axios.defaults.withCredentials = true;
 axios.defaults.transformRequest = (data = {}) => Qs.stringify(data);
 axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
